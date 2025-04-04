8f95ff146c2c5aba1cb01e7fc944ffd58723a90e
RamenEnjoyer69 <sothearithmeng123@gmail.com>
2025-04-04 10:20:13 +0700

    removed static components

--- src/components/sidebar/SidebarFavoriteList.jsx
+++ src/components/sidebar/SidebarFavoriteList.jsx
@@ -9,56 +9,16 @@ const SidebarFavoriteList = () => {
   return (
     <ScrollArea className="h-[250px]">
       <ul className="flex flex-col mt-4 ">
-        {/* <p className="mt-4 text-gray-400 px-4">Your favorite list is empty</p> */}
+        <p className="mt-4 text-gray-400 px-4">Your favorite list is empty</p>
 
-        <li className="flex items-center justify-between p-4 rounded-2xl hover:bg-gray-100 cursor-pointer text-lg">
+        {/* <li className="flex items-center justify-between p-4 rounded-2xl hover:bg-gray-100 cursor-pointer text-lg">
           <div className="flex items-center gap-4">
             <div className="rounded-full bg-watermelon-red w-3 h-3 "></div>
             <p>Mobile Design</p>
           </div>
 
           <MoreHorizontalCircle01Icon />
-        </li>
-        <li className="flex items-center justify-between p-4 rounded-2xl hover:bg-gray-100 cursor-pointer text-lg">
-          <div className="flex items-center gap-4">
-            <div className="rounded-full bg-watermelon-red w-3 h-3 "></div>
-            <p>Mobile Design</p>
-          </div>
-
-          <MoreHorizontalCircle01Icon />
-        </li>
-        <li className="flex items-center justify-between p-4 rounded-2xl hover:bg-gray-100 cursor-pointer text-lg">
-          <div className="flex items-center gap-4">
-            <div className="rounded-full bg-watermelon-red w-3 h-3 "></div>
-            <p>Mobile Design</p>
-          </div>
-
-          <MoreHorizontalCircle01Icon />
-        </li>
-        <li className="flex items-center justify-between p-4 rounded-2xl hover:bg-gray-100 cursor-pointer text-lg">
-          <div className="flex items-center gap-4">
-            <div className="rounded-full bg-watermelon-red w-3 h-3 "></div>
-            <p>Mobile Design</p>
-          </div>
-
-          <MoreHorizontalCircle01Icon />
-        </li>
-        <li className="flex items-center justify-between p-4 rounded-2xl hover:bg-gray-100 cursor-pointer text-lg">
-          <div className="flex items-center gap-4">
-            <div className="rounded-full bg-watermelon-red w-3 h-3 "></div>
-            <p>Mobile Design</p>
-          </div>
-
-          <MoreHorizontalCircle01Icon />
-        </li>
-        <li className="flex items-center justify-between p-4 rounded-2xl hover:bg-gray-100 cursor-pointer text-lg">
-          <div className="flex items-center gap-4">
-            <div className="rounded-full bg-watermelon-red w-3 h-3 "></div>
-            <p>Mobile Design</p>
-          </div>
-
-          <MoreHorizontalCircle01Icon />
-        </li>
+        </li> */}
       </ul>
     </ScrollArea>
   );
